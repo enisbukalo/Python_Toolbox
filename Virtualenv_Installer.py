a6bec191adbff5908177da63b8adf8531ee80e4b
Enis Bukalo <bukaloenis@gmail.com>
2018-11-01 12:36:20 -0400

    Update

--- Virtualenv_Installer.py
+++ Virtualenv_Installer.py
@@ -3,6 +3,7 @@ This Module will handle the creation of a virtual environment at
 the given location with the given name.
 """
 from subprocess import Popen
+from os.path import isfile
 
 
 class VirtualenvInstaller():
@@ -14,12 +15,24 @@ class VirtualenvInstaller():
         self.InitCreation()
 
     def InitCreation(self):
+        """Check the existance of the Python.exe file"""
+        def CheckPath():
+            if isfile(self.python):
+                return True
+            else:
+                return False
+
+        result_ = CheckPath()
         currwd = self.destination
         if self.python is None or self.python == "":
             command_to_send = ('virtualenv {0}').format(self.name)
         else:
-            command_to_send = ('virtualenv --python={0} {1}').format(
-                    self.python,
-                    self.name)
+            if result_ is True:
+                command_to_send = ('virtualenv --python={0} {1}').format(
+                        self.python,
+                        self.name)
+            else:
+                command_to_send = ('virtualenv {0}').format(self.name)
+
         p = Popen(command_to_send, cwd=currwd)
         p.wait()
